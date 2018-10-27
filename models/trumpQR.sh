d358641d4f2b417aae085eaee626998c77797dd0
airvex888 <airvex@utexas.edu>
2018-10-27 14:17:03 -0500

    Added quadrant code and new .dae files.

--- models/trumpQR.sh
+++ models/trumpQR.sh
@@ -1,12 +1,20 @@
-	# Generates a pseudorandom QR Code. 
+# Generates a QR Code from user input. 
+	echo "Type in the 4 digits of the QRCode"
+	read i
 
-	j=$((1 + RANDOM % 9))
-	echo $"j = "$j
-	k=$((1 + RANDOM % 9))
-	echo $"k = "$k
-	l=$((1 + RANDOM % 9))
-	echo $"l = "$l
-	m=$((1 + RANDOM % 9))
-	echo $"m = "$m
-	qrencode -o qrcode.png $j$k$l$m
-	cd ..
+	height=145              # height of cropped image
+	width=145               # width of cropped image
+
+	
+	# Generate top left image
+	qr "$i" | convert - -crop "$height"x"$width"+0+0 qr_topLeft.png
+
+	# Generate top right image
+	qr "$i" | convert - -crop "$height"x"$width"+"$width"+0 qr_topRight.png
+
+	# Generate bottom left image
+	qr "$i" | convert - -crop "$height"x"$width"+0+"$height" qr_bottomLeft.png
+
+	# Generate bottom right image
+	qr "$i" | convert - -crop "$height"x"$width"+"$width"+"$height" qr_bottomRight.png
+	
